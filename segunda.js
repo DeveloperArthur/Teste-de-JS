107dcb63493279836ee1e251df23ecf132f8544c
DoisLucas <pedrolucas499@gmail.com>
2019-05-15 09:30:38 -0300

    Correção segunda questão

--- segunda.js
+++ segunda.js
@@ -1,18 +1,26 @@
 /*2) Faça um programa que identifique se em um determinado vetor, existem ou não valores repetidos.*/
 
-function segunda(){
-    var array = [1,0,2,8,3,1];
-    var cont = 0;
-    var repetido = false;
-    
-    for(var i=0; i<array.length; i++){
-        for(var k = 1; k < array.length - 1; k++ ){
-            if(array[i] === array[k])
-                repetido = true;
-                break;
+function segunda() {
+
+    var array = [1, 0, 2, 8, 3, 7]
+    var repetido = false
+
+    for (var i = 0; i < array.length; i++) {
+        for (var k = 0; k < array.length; k++) {
+
+            if (i !== k) {
+                if (array[i] === array[k]) {
+                    repetido = true
+                    break
+                }
+            }
+
         }
+
+        if (repetido)
+            break
     }
-    
-    repetido ?  console.log("Existem valores repetidos") : console.log("Não existem valores repetidos");
-        
+
+    repetido ? console.log("Existem valores repetidos") : console.log("Não existem valores repetidos")
+
 }
